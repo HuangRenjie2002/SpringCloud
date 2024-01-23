e19ddaf602c0419e85d42ec62d6a1a8a5384f070
HuangRenjie <huangrenjie2002@qq.com>
2024-01-23 10:28:00 +0800

    add RestVo

---
+++ common/src/main/java/pers/huang/enums/ResultCodeEnum.java
@@ -0,0 +1,41 @@
+package pers.huang.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+import java.util.stream.Stream;
+
+/**
+ * @Author: HuangRenjie
+ * @Date: 2023/1/23 22:49
+ * @Description:
+ */
+@Getter
+public enum ResultCodeEnum {
+    SUCCESS("0000", "操作成功"),
+    FAILED("0001", "操作失败"),
+    TOKEN_FAILURE("0002","身份信息验证失败，请重新登陆")
+    ;
+
+    private String code;
+    private String msg;
+
+    ResultCodeEnum(String code, String msg) {
+        this.msg = msg;
+        this.code = code;
+    }
+    public String msg() {
+        return this.msg;
+    }
+
+    public String code() {
+        return this.code;
+    }
+
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static ResultCodeEnum fromCode(Integer code) {
+        return Stream.of(ResultCodeEnum.values()).filter(e -> e.code.equals(code)).findFirst().orElse(null);
+    }
+}
+
