9d78ea2f75ad795af3af913b9e6d8f0fdaafab73
HuangRenjie <huangrenjie2002@qq.com>
2024-02-20 09:57:54 +0800

    RabbitMq手动ack

--- consumer/src/main/java/pers/huang/consumer/listener/TestListener.java
+++ consumer/src/main/java/pers/huang/consumer/listener/TestListener.java
@@ -1,16 +1,22 @@
 package pers.huang.consumer.listener;
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import com.alibaba.fastjson2.JSONObject;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import pers.huang.model.vo.UserVo;
 
-@Component
+import java.io.IOException;
 
+@Configuration
 public class TestListener {
     @RabbitListener(queues = "Queue")
-    public void test(UserVo user) {
-        System.out.println(user);
+    public void test(Message msg, Channel channel) throws IOException {
+        byte[] body = msg.getBody();
+        UserVo userVo = JSONObject.parseObject(new String(body), UserVo.class);
+        System.out.println(userVo);
+        channel.basicAck(msg.getMessageProperties().getDeliveryTag(),true);
     }
 }
 
